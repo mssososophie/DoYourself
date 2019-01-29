f2a9bcbfeb26464701ddae630753ee39d87087b1
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2019-01-29 20:23:10 +0000

    completing setup and building out a cleaner way to define the dot and dash variables

--- DoYourself.ino
+++ DoYourself.ino
@@ -1,8 +1,9 @@
 //needs to be onbuttonpress()
 
 void setup() {
-  // put your setup code here, to run once:
-pinMode(numberofpin, )
+  Serial.begin(9600);
+  pinMode(numberofpin, OUTPUT); //for vibration component
+  pinMode(numberofpin, OUTPUT); //for LED
 }
 
 void loop() {
@@ -19,3 +20,25 @@ void dot () {digitalWrite(numberofpin,225); delay (300); digitalWrite(vibrate,LO
 void dash () {digitalWrite(numberofpin,225); delay (900); digitalWrite(vibrate,LOW); delay (300);}//the dash this code make the led on for 900 than off for 300
 void shortspace () {delay(600);}//space between letters
 void space () {delay (1200);}//space between words
+
+void dot(){
+  digitalWrite(numberofpin, HIGH); //for vibration
+  digitalWrite(numberofpin, HIGH); //for LED
+  delay(50); //leaves above pins on for 100 milliseconds
+  digitalWrite(numberofpin, LOW); //for vibration
+  digitalWrite(numberofpin, LOW); //for LED
+  delay(100);
+  }
+
+  void dash(){
+  digitalWrite(numberofpin, HIGH); //for vibration
+  digitalWrite(numberofpin, HIGH); //for LED
+  delay(150); //leaves above pins on for 300 milliseconds
+  digitalWrite(numberofpin, LOW); //for vibration
+  digitalWrite(numberofpin, LOW); //for LED
+  delay(100);
+  }
+
+
+//TO DO
+//instead of HIGH/LOW for vibration component find out how it works
